077eac586845c1288d6ca30ed59a2d282eeb6a00
Daolinzhou <466118115@qq.com>
2019-04-22 21:21:51 -0700

    add eureka and zuul gateway

---
+++ ad-eureka/src/main/java/com/daolin/ad/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.daolin.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Create by Daolin
+ * on 04 / 2019
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
